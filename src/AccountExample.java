ee02cff88df525830894de43183b28f169c761f7
unknown <phamvietcuong12@gmail.com>
2021-10-13 15:59:05 +0700

    validate account

---
+++ src/AccountExample.java
@@ -0,0 +1,22 @@
+import com.sun.org.apache.xerces.internal.impl.xpath.regex.Match;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class AccountExample {
+    /*Không chứa các ký tự đặc biệt
+	  Phải dài hơn 6 ký tự
+	  Không chứa các ký tự viết hoa
+	  Cho phép dấu gạch dưới (_)
+	*/
+    public static final String ACCOUNT_REGEX ="^[_a-z0-9]{6,}$";
+    public AccountExample(){
+    }
+
+    public boolean validate(String regex){
+        Pattern pattern = Pattern.compile(ACCOUNT_REGEX);
+        Matcher matcher = pattern.matcher(regex);
+        return matcher.matches();
+    }
+}
+
